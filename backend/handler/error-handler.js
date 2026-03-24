3b8b0e0fac545f10eff89d6d65267ea69f089741
elliotracinep <racinenmimie1@gmail.com>
2026-03-24 18:37:35 -0400

    ajout de l'errorHandler

---
+++ backend/handler/error-handler.js
@@ -0,0 +1,10 @@
+function errorHandler(error, req, res, next) {
+  if (res.headersSent) {
+    return next(error);
+  }
+  // le code de l'erreur est renvoyé :
+  res.status(error.code || 500);
+  // le message de l'erreur est renvoyé :
+  res.json({ message: error.message || "Une erreur inconnue est survenue !" });
+}
+export default errorHandler;
