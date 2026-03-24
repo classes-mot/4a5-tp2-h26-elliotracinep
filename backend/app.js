94bae9384065cceaa6c1619c144de2de27489aa7
elliotracinep <racinenmimie1@gmail.com>
2026-03-24 19:35:29 -0400

    ajustement dans app.js

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import gameRoutes from "./routes/game-routes.js";
+import userRoutes from "./routes/user-routes.js";
 import errorHandler from "./handler/error-handler.js";
 import cors from "cors";
 
@@ -9,8 +10,10 @@ app.use(cors());
 app.use(express.json());
 
 // les routes
+// pour les jeux :
 app.use("/api/jeux", gameRoutes);
-
+// pour les utilisateurs :
+app.use("/api/users", userRoutes);
 // gestion si la route n'existe pas
 app.use((req, res, next) => {
   const erreur = new Error("La route est introuvable !");
