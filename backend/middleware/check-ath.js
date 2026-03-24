6320c764d7f0b5965b0932b98cc7f4c0f0fc1366
elliotracinep <racinenmimie1@gmail.com>
2026-03-24 18:46:03 -0400

    ajout du check-auth

---
+++ backend/middleware/check-ath.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+import HttpError from "../util/http-error";
+
+const checkAuth = (res, req, next) => {
+  try {
+    // création du jeton d'identification
+    const token = req.headers.authorization.split(" ")[1];
+    if (!token) {
+      throw new Error("L'authentification a échoué !");
+    }
+    const decodedToken = jwt.verify(
+      token,
+      "Clé3987snb(*&nDe90238nsdbmsnnSignature!!!!ero837vsc*(&(:5e",
+    );
+    req.userData = { userId: decodedToken.userId };
+    next();
+  } catch (error) {
+    console.log("Une erreur est survenue : ", error);
+    const monErreur = new HttpError("L'authentification a échoué !", 401);
+    return next(monErreur);
+  }
+};
+export default checkAuth;
+// gestion du jeton d'authentification
