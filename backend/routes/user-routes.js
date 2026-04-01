c97ff21475522833674ae0913b78bf1d0cb7f37f
elliotracinep <racinenmimie1@gmail.com>
2026-04-01 13:04:15 -0400

    sauvegarde

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -1,10 +1,19 @@
 import express from "express";
 import usersControllers from "../controllers/users-controllers.js";
+import { check } from "express-validator";
 
 const userRoutes = express.Router();
 
 // route pour l'enregistrement d'un utilisateur :
-userRoutes.post("/register", usersControllers.registerUser);
+userRoutes.post(
+  "/register",
+  [
+    check("username").not().isEmpty(),
+    check("email").not().isEmpty(),
+    check("password").not().isEmpty(),
+  ],
+  usersControllers.registerUser,
+);
 // route pour la connexion d'un utilisateur :
 userRoutes.post("/login", usersControllers.login);
 
